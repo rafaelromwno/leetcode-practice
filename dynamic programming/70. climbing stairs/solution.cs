603c7539c93f979358f12dcef29e1e4bdc017de9
Rafael Romano Silva <165402214+rafaelromwno@users.noreply.github.com>
2025-08-27 11:29:52 -0300

    feat: 70. climbing stairs

---
+++ dynamic programming/70. climbing stairs/solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int first = 1, second = 2, result = 0;
+
+        for (int i = 3; i <= n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+
+        return second;
+    }
+}
